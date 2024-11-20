4b5b37d751dd264f555a3a6e5e6106a0c063bb49
Andre Gabriel Valverde Mozo <u202218899@upc.edu.pe>
2024-11-19 20:42:42 -0500

    feat: New DataBase

--- ACME.CargoExpress.API/obj/Debug/net8.0/ACME.CargoExpress.API.AssemblyInfo.cs
+++ ACME.CargoExpress.API/obj/Debug/net8.0/ACME.CargoExpress.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ACME.CargoExpress.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c50c0c0e591a8267e81d3d826ba5cd6d589e1cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4dd853af49a4c24fb836283276da5cdd1fff3e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ACME.CargoExpress.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ACME.CargoExpress.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CargoExpress.UnitTests/obj/Debug/net8.0/CargoExpress.UnitTests.AssemblyInfo.cs
+++ CargoExpress.UnitTests/obj/Debug/net8.0/CargoExpress.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CargoExpress.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c50c0c0e591a8267e81d3d826ba5cd6d589e1cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4dd853af49a4c24fb836283276da5cdd1fff3e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CargoExpress.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CargoExpress.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
